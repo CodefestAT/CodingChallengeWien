ad9785230dccafea70f3b25219524363a199bf88
Rina Ahmed <rina.ahmed@live.com>
2015-11-13 14:51:33 +0100

    Challenge 2

--- CodingChallengeWien/Challenges/Challenge02.cs
+++ CodingChallengeWien/Challenges/Challenge02.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 //2.	Erstellen Sie eine Funktion, die überprüft, ob der übergebene String eine gültige Mac Adresse ist(Beispiel: 00-80-41-ae-fd-7e oder 00:80:41:ae:fd:7e).
 //Tipp: https://de.wikipedia.org/wiki/MAC-Adresse#Form_.28Syntax.29_von_MAC-Adressen
@@ -13,11 +13,10 @@ namespace CodingChallengeWien.Challenges
 {
     class Challenge02
     {
-
         internal static bool MacBerechnung(string macAdresse)
         {
-            //write checking algo
-            return false;
+            var expression = @"(([a-f]|[0-9]|[A-F]){2}[-\:]){5}([a-f]|[0-9]|[A-F]){2}\b";
+            return System.Text.RegularExpressions.Regex.IsMatch(macAdresse, expression);
         }
     }
 }
